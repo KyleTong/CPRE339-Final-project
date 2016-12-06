99bad89dd7bd21619965fa2a49de7994f990b623
Xin Tong <xint@iastate.edu>
2016-12-06 01:28:32 -0600

    xin MachineActivity.java

---
+++ MachineActivity.java
@@ -0,0 +1,13 @@
+package com.application.kyle.infinet_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MachineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_machine);
+    }
+}
